3fc933b21331d64bba9e786482223998930dd312
Zumin Li <zumin.li.work@gmail.com>
2026-07-14 20:24:26 +0800

    fix metrics permission

--- src/main/java/top/flyingjack/cashier/config/ResourceServerConfig.java
+++ src/main/java/top/flyingjack/cashier/config/ResourceServerConfig.java
@@ -25,7 +25,9 @@ public class ResourceServerConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-            .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/actuator/**").permitAll()
+                .anyRequest().authenticated())
             .oauth2ResourceServer(oauth2 -> oauth2
                 .jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter)))
             .csrf(csrf -> csrf.disable())
